ee9b3862a1b10281efb5c1f79bf7103848371b78
Shawn315 <shawnbasketball3@yahoo.com>
2016-04-21 21:05:34 -0500

    Added files via upload

--- SpeedOfRotation.c
+++ SpeedOfRotation.c
@@ -11,6 +11,8 @@
 #define HOLES 32                        //number of holes in each wheel
 int rightWheelSpeed= 0;                 //global variable to track right wheel speed
 int leftWheelSpeed= 0;                  //global variable to track left wheel speed
+int leftCountTracker = 0;               //keeps track of past counts for instantaneous speed calculation
+int rightCountTracker = 0;              //keeps track of past counts for instantaneous speed calculation
 unsigned long leftWheelCount = 0;       //global variable to track hole/space count of left wheel
 unsigned long rightWheelCount = 0;      //global variable to track hole/space count of right wheel
 unsigned long elapsedMillis = 0;        //global variable to track time ticks in milliseconds
@@ -18,17 +20,22 @@ unsigned long speedElapsedMillis = 0;   //global variable to track instantaneous
 unsigned long rightWheelDistance = 0;   //global variable to track right wheel distance
 unsigned long leftWheelDistance = 0;    //global variable to track left wheel distance
 
+
 void main(void) 
 {
     while (1)
     {
         //5.3996 is the millimeters per count for the wheel
         //tracks the instantaneous right wheel speed
-        rightWheelSpeed = rightWheelCount * 5.3996 / (elapsedMillis - speedElapsedMillis); 
+        rightWheelSpeed = (rightWheelCount - rightCountTracker) * 5.3996 / (elapsedMillis - speedElapsedMillis); 
         //tracks the instantaneous left wheel speed
-        leftWheelSpeed = leftWheelCount * 5.3996 / (elapsedMillis - speedElapsedMillis);   
+        leftWheelSpeed = (leftWheelCount - leftCountTracker) * 5.3996 / (elapsedMillis - speedElapsedMillis);   
         //sets the speedElapsedMillis the same as the current time tick
         speedElapsedMillis = elapsedMillis;
+        //sets the right count tracker the same as right wheel count
+        rightCountTracker = rightWheelCount;
+        //sets the left count tracker the same as left wheel count
+        leftCountTracker = leftWheelCount;
         //This tracks the right wheel distance in millimeters
         rightWheelDistance = rightWheelCount * 5.3996;
         //This tracks the left wheel distance in millimeters
